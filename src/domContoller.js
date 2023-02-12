0bbbf7a357fbd90c30fa99804c0f7e763a430f50
Berk <mikeberk93@gmail.com>
2023-02-12 11:07:01 -0600

    migrated functions to appropriate modules and finalized delete project functionality

--- src/domContoller.js
+++ src/domContoller.js
@@ -26,6 +26,8 @@ const domStuff = (() => {
         if (globalVars.currentDisplay == projectToDelete) {
             domRender.displayTasks(globalVars.tasksArr.sort((a, b) => a.taskAdded - b.taskAdded));
         }
+        const projects = document.querySelectorAll('.project-name')
+        projects.forEach(proj => proj.addEventListener('click', switchProject));
     }
 
     const addTaskToDOM = (task) => {
@@ -156,6 +158,123 @@ const domStuff = (() => {
         return taskContainer;
     }
 */
+    const deleteTask = (e) => {
+        let taskName = e.target.dataset.delete;
+        let taskIndex = globalVars.tasksArr.map(e => e.title).indexOf(taskName);
+        globalVars.tasksArr.splice(taskIndex, 1);
+
+        if (globalVars.currentDisplay !== 'default') {
+            let currentProjectTasks = globalVars.tasksArr.filter(task => task.project == globalVars.currentDisplay);
+            domRender.displayTasks(currentProjectTasks);
+        } else {
+            globalVars.tasksArr.sort((a, b) => a.taskAdded - b.taskAdded);
+            domRender.displayTasks(globalVars.tasksArr);
+        }
+    
+        const deleteBtns = document.querySelectorAll(".delete-btn");
+        deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
+
+        const editBtns = document.querySelectorAll(".edit-btn");
+        editBtns.forEach(btn => btn.addEventListener('click', editTask));
+    }
+
+    const editTask = (e) => {
+        let taskName = e.target.dataset.edit;
+        let taskIndex = globalVars.tasksArr.map(e => e.title).indexOf(taskName);
+        let task = globalVars.tasksArr[taskIndex];
+        e.target.textContent = 'Save'
+        e.target.removeEventListener('click', editTask);
+
+        const itemDiv = e.target.parentElement;
+        itemDiv.focus();
+
+        const titleInput = itemDiv.querySelector('input[name=title]');
+        const descInput = itemDiv.querySelector('input[name=desc]');
+        const dueInput = itemDiv.querySelector('input[name=due]');
+        const select = itemDiv.querySelector('select');
+        const deleteButton = itemDiv.querySelector('.delete-btn');
+        const priorityIndicator = itemDiv.querySelector('.indicator');
+        const projectInput = itemDiv.querySelector('input[name=project]');
+
+        titleInput.removeAttribute('readonly');
+        descInput.removeAttribute('readonly');
+        dueInput.removeAttribute('readonly');
+        projectInput.removeAttribute('readonly');
+        projectInput.style.fontStyle = 'normal';
+        projectInput.style.fontWeight = 'normal';
+
+        dueInput.hidden = false;
+        priorityIndicator.hidden = true;
+        select.removeAttribute('disabled');
+        select.classList.remove('hidden');
+        select.removeAttribute('hidden');
+
+        e.target.addEventListener('click', (e) => {
+            e.preventDefault();
+            task._title = titleInput.value;
+            task.description = descInput.value;
+            task.dueDate = dueInput.value;
+            task.priority = select.value;
+
+            if (task.project !== projectInput.value) {
+                if (!globalVars.projectsArr.includes(projectInput.value)) {
+                    globalVars.projectsArr.push(projectInput.value);
+                }
+                domRender.displayProjects(globalVars.projectsArr);
+                const projects = document.querySelectorAll('.project-name')
+                projects.forEach(proj => proj.addEventListener('click', switchProject));
+                // const wrappers = document.querySelectorAll('project-wrapper');
+                // wrappers.forEach(div => div.addEventListener('mouseover', toggleTrashCan));
+                // wrappers.forEach(div => div.addEventListener('mouseleave', toggleTrashCan));
+            } 
+            task.project = projectInput.value;
+
+            titleInput.readOnly = true;
+            descInput.readOnly = true;
+            dueInput.readOnly = true;
+            if (dueInput.value === '') {
+                dueInput.hidden = true;
+            }
+            select.disabled = true;
+            select.classList.add('hidden');
+            select.hidden = true;
+            projectInput.readOnly = true;
+            projectInput.style.fontStyle = 'italic';
+            projectInput.style.fontWeight = 'bold';
+            
+            priorityIndicator.textContent = task.priority == 'high' ? '!!!'
+            : task.priority == 'medium' ? '!!'
+            : task.priority == 'low' ? '!'
+            : '';
+            priorityIndicator.hidden = false;
+
+            e.target.textContent = 'Edit';
+            e.target.dataset.edit = task.title;
+            deleteButton.dataset.delete = task.title;
+            e.target.addEventListener('click', editTask);
+            // need to add the project input
+            // re-render tasks on save or just make all attributes readonly and swap the event listener on the save/edit btn?
+            
+        })
+    }
+
+    const switchProject = (e) => {
+        let projectClicked = e.target.textContent;
+        if (projectClicked == 'All Tasks') {
+            globalVars.tasksArr.sort((a, b) => a.taskAdded - b.taskAdded);
+            domRender.displayTasks(globalVars.tasksArr);
+            globalVars.currentDisplay = 'default';
+        } else {
+            domRender.displayTasks(globalVars.tasksArr.filter(t => t.project == projectClicked));
+            globalVars.currentDisplay = projectClicked;
+        }
+        
+        const deleteBtns = document.querySelectorAll(".delete-btn");
+        deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
+        const editBtns = document.querySelectorAll(".edit-btn");
+        editBtns.forEach(btn => btn.addEventListener('click', editTask));
+    }
+
     const addProjectToDOM = (project) => {
         //project.name
         let projectElemWrapper = createHtmlElement('div', '', 'project-wrapper');
@@ -183,9 +302,12 @@ const domStuff = (() => {
         return projectElemWrapper;
     }
 
+
     return {
         addTaskToDOM,
-        addProjectToDOM
+        addProjectToDOM,
+        editTask,
+        deleteTask
     }
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Task } from "./constructors";
 import { format } from "date-fns";
 import { domRender } from "./domRender";
+import { domStuff } from "./domContoller";
 import globalVars from './globals';
 
 const newTaskForm = document.querySelector('.new-task-form');
@@ -24,10 +25,10 @@ globalVars.projectsArr.push('exercise');
 domRender.displayProjects(globalVars.projectsArr);
 domRender.displayTasks(globalVars.tasksArr);
 const deleteBtns = document.querySelectorAll(".delete-btn");
-deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
+deleteBtns.forEach(btn => btn.addEventListener('click', domStuff.deleteTask));
 
 const editBtns = document.querySelectorAll(".edit-btn");
-editBtns.forEach(btn => btn.addEventListener('click', editTask));
+editBtns.forEach(btn => btn.addEventListener('click', domStuff.editTask));
 
 const createTask = (e) => {
     e.preventDefault();
@@ -60,10 +61,10 @@ const createTask = (e) => {
         globalVars.currentDisplay = 'default';
     }
     const deleteBtns = document.querySelectorAll(".delete-btn");
-    deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
+    deleteBtns.forEach(btn => btn.addEventListener('click', domStuff.deleteTask));
 
     const editBtns = document.querySelectorAll(".edit-btn");
-    editBtns.forEach(btn => btn.addEventListener('click', editTask));
+    editBtns.forEach(btn => btn.addEventListener('click', domStuff.editTask));
     
     newTaskForm.reset();
     toggleForm();
@@ -75,106 +76,6 @@ const createTask = (e) => {
 
 newTaskForm.addEventListener('submit', createTask);
 
-function deleteTask(e) {
-    let taskName = e.target.dataset.delete;
-    let taskIndex = globalVars.tasksArr.map(e => e.title).indexOf(taskName);
-    globalVars.tasksArr.splice(taskIndex, 1);
-
-    if (globalVars.currentDisplay !== 'default') {
-        let currentProjectTasks = globalVars.tasksArr.filter(task => task.project == globalVars.currentDisplay);
-        domRender.displayTasks(currentProjectTasks);
-    } else {
-        globalVars.tasksArr.sort((a, b) => a.taskAdded - b.taskAdded);
-        domRender.displayTasks(globalVars.tasksArr);
-    }
- 
-    const deleteBtns = document.querySelectorAll(".delete-btn");
-    deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
-
-    const editBtns = document.querySelectorAll(".edit-btn");
-    editBtns.forEach(btn => btn.addEventListener('click', editTask));
-}
-
-function editTask(e) {
-    let taskName = e.target.dataset.edit;
-    let taskIndex = globalVars.tasksArr.map(e => e.title).indexOf(taskName);
-    let task = globalVars.tasksArr[taskIndex];
-    e.target.textContent = 'Save'
-    e.target.removeEventListener('click', editTask);
-
-    const itemDiv = e.target.parentElement;
-    itemDiv.focus();
-
-    const titleInput = itemDiv.querySelector('input[name=title]');
-    const descInput = itemDiv.querySelector('input[name=desc]');
-    const dueInput = itemDiv.querySelector('input[name=due]');
-    const select = itemDiv.querySelector('select');
-    const deleteButton = itemDiv.querySelector('.delete-btn');
-    const priorityIndicator = itemDiv.querySelector('.indicator');
-    const projectInput = itemDiv.querySelector('input[name=project]');
-
-    titleInput.removeAttribute('readonly');
-    descInput.removeAttribute('readonly');
-    dueInput.removeAttribute('readonly');
-    projectInput.removeAttribute('readonly');
-    projectInput.style.fontStyle = 'normal';
-    projectInput.style.fontWeight = 'normal';
-
-    dueInput.hidden = false;
-    priorityIndicator.hidden = true;
-    select.removeAttribute('disabled');
-    select.classList.remove('hidden');
-    select.removeAttribute('hidden');
-
-    e.target.addEventListener('click', (e) => {
-        e.preventDefault();
-        task._title = titleInput.value;
-        task.description = descInput.value;
-        task.dueDate = dueInput.value;
-        task.priority = select.value;
-
-        if (task.project !== projectInput.value) {
-            if (!globalVars.projectsArr.includes(projectInput.value)) {
-                globalVars.projectsArr.push(projectInput.value);
-            }
-            domRender.displayProjects(globalVars.projectsArr);
-            const projects = document.querySelectorAll('.project-name')
-            projects.forEach(proj => proj.addEventListener('click', switchProject));
-            // const wrappers = document.querySelectorAll('project-wrapper');
-            // wrappers.forEach(div => div.addEventListener('mouseover', toggleTrashCan));
-            // wrappers.forEach(div => div.addEventListener('mouseleave', toggleTrashCan));
-        } 
-        task.project = projectInput.value;
-
-        titleInput.readOnly = true;
-        descInput.readOnly = true;
-        dueInput.readOnly = true;
-        if (dueInput.value === '') {
-            dueInput.hidden = true;
-        }
-        select.disabled = true;
-        select.classList.add('hidden');
-        select.hidden = true;
-        projectInput.readOnly = true;
-        projectInput.style.fontStyle = 'italic';
-        projectInput.style.fontWeight = 'bold';
-        
-        priorityIndicator.textContent = task.priority == 'high' ? '!!!'
-        : task.priority == 'medium' ? '!!'
-        : task.priority == 'low' ? '!'
-        : '';
-        priorityIndicator.hidden = false;
-
-        e.target.textContent = 'Edit';
-        e.target.dataset.edit = task.title;
-        deleteButton.dataset.delete = task.title;
-        e.target.addEventListener('click', editTask);
-        // need to add the project input
-        // re-render tasks on save or just make all attributes readonly and swap the event listener on the save/edit btn?
-        
-    })
-}
-
 function toggleForm() {
     if (globalVars.currentDisplay !== 'default') {
         document.getElementById('project').value = globalVars.currentDisplay;
@@ -185,27 +86,6 @@ function toggleForm() {
     domRender.toggleForm(newTaskDiv);
 }
 
-function switchProject(e) {
-    let projectClicked = e.target.textContent;
-    if (projectClicked == 'All Tasks') {
-        globalVars.tasksArr.sort((a, b) => a.taskAdded - b.taskAdded);
-        domRender.displayTasks(globalVars.tasksArr);
-        globalVars.currentDisplay = 'default';
-    } else {
-        domRender.displayTasks(globalVars.tasksArr.filter(t => t.project == projectClicked));
-        globalVars.currentDisplay = projectClicked;
-    }
-    
-    const deleteBtns = document.querySelectorAll(".delete-btn");
-    deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask));
-    const editBtns = document.querySelectorAll(".edit-btn");
-    editBtns.forEach(btn => btn.addEventListener('click', editTask));
-}
-
-// move this to domRender.js after local storage buildout
-
-
-
 // REMAINING TASKS
 // +delete projects
 //  |__delete btn that shows up on hover. if tasks are in project, remove the project name?
@@ -213,3 +93,4 @@ function switchProject(e) {
 // add icons to project list
 // delete btn should be red
 // local storage functionality
+// the backgrounds don't extend when tasks go off page (change 100vh to 100%?)
